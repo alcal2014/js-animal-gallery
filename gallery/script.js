25a4069bd89c965dc97e7f5b71c47223f944efa5
alain <profitable@gmail.com>
2014-10-07 20:35:37 -0500

    Fetch JSON data and parse into page

--- gallery/script.js
+++ gallery/script.js
@@ -1,7 +1,7 @@
 // Add your code here
 $(document).ready(function() {
 
-	$('.filter-button').click(function() {
+	$('body').on('click','.filter-button',function() {
 
 	  	$('.' + this.id).toggle();
 
@@ -9,4 +9,32 @@ $(document).ready(function() {
 
 	});
 
+
+	$.getJSON( "https://mks-frontend-gallery.herokuapp.com/", function( json ) {
+
+		var animalarr = [];
+
+		$.each( json, function( x, val ) {
+	  		
+	  		$('.gallery').append('<div id="'+x+'"" class="large-4 small-6 columns"><img src="'+val.url+'"></div>');
+
+		    $.each(val.animals, function(i,animal) {
+		    		$('.columns #' + x).addClass(animal);
+
+		    		if(jQuery.inArray( animal, animalarr ) == -1) {
+		    			animalarr.push(animal);
+		    		}
+		    });
+ 
+		});
+	
+
+		$.each(animalarr, function(i,animalid) {
+		  	$('.filters').append('<div class="filter-button individual button active" id="'+animalid+'">'+animalid+'</div>');
+		});
+
+	});
+
+
+	
 });
