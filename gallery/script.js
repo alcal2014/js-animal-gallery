975761c66ab789376ab5ee65a24ffc34ab54c569
alain <profitable@gmail.com>
2014-10-07 19:15:51 -0500

    Toggle classes

--- gallery/script.js
+++ gallery/script.js
@@ -1,1 +1,12 @@
 // Add your code here
+$(document).ready(function() {
+
+	$('.filter-button').click(function() {
+
+	  	$('.' + this.id).toggle();
+
+	    $(this).toggleClass('active');
+
+	});
+
+});
